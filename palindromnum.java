a736132c0d9fbd1f1f2f63daa62a83cec30491b4
shreyasebone <101252000+shreyasebone@users.noreply.github.com>
2022-03-10 20:57:27 +0530

    Update palindromnum.java

--- palindromnum.java
+++ palindromnum.java
@@ -2,23 +2,27 @@ package shreya;
 import java.util.*;
 
 public class palindromnum {
-
+      public void palindrome(int num) {
+    	  int temp=num;
+  		int sum=0,rev=0;
+  		while(num>0) {
+  			rev=num%10;
+  			sum=sum*10+rev;
+  			num=num/10;
+  		}
+  		if(temp == sum) {
+  			System.out.println("number is palindrom");
+  		}else {
+  			System.out.println("number is not palindrom");
+  		}
+      }
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter a number");
 		int num=sc.nextInt();
-		int temp=num;
-		int sum=0,rev=0;
-		while(num>0) {
-			rev=num%10;
-			sum=sum*10+rev;
-			num=num/10;
-		}
-		if(temp == sum) {
-			System.out.println("number is palindrom");
-		}else {
-			System.out.println("number is not palindrom");
-		}
+		palindromnum p=new palindromnum();
+		p.palindrome(num);
+		
 	}
 
 }
