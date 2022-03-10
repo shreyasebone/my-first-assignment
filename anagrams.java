3d70219998315800abab076d64d1847ae426a5ff
shreyasebone <101252000+shreyasebone@users.noreply.github.com>
2022-03-10 20:53:07 +0530

    Update anagrams.java

--- anagrams.java
+++ anagrams.java
@@ -1,30 +1,35 @@
 package shreya;
 import java.util.*;
-public class anagrams {
 
+public class primenum {
+    public void prime(int n) {
+    	 int i,j,flag;
+    	 for(i=0;i<=n;i++) {
+    		 if (i==1||i==0) {
+    			 continue;
+    		 }
+    		 flag=1;
+    		 for(j=2;j<=i/2;j++) {
+    			 if(i%j==0) {
+    				 flag=0;
+    				 break;
+    			 }
+    		 }
+    		 if(flag==1) {
+    			 System.out.println(i);
+    		 }
+    }
+    }
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter first string");
-		String s1=sc.next();
-		
-		System.out.println("enter second string");
-		String s2=sc.next();
-		
-		char a[]=s1.toLowerCase().toCharArray();
-		char b[]=s2.toLowerCase().toCharArray();
-		
-		Arrays.sort(a);
-		Arrays.sort(b);
-		boolean result=Arrays.equals(a,b);
-		if(result==true)
-		{
-			System.out.println("string is anagram");
-			
-		}else
-		{
-			System.out.println("string is not anagram");
-		}
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("enter a number");
+	 int nu=sc.nextInt();
+	 primenum p=new primenum();
+	 p.prime(nu);
 
+	 }
+	
 	}
 
-}
+
+
