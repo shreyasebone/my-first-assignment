3ffe59a746fb292ed94f1296f5226daf1cd59998
shreyasebone <101252000+shreyasebone@users.noreply.github.com>
2022-03-10 20:55:03 +0530

    Update occur.java

--- occur.java
+++ occur.java
@@ -1,17 +1,11 @@
 package shreya;
 import java.util.*;
 
-
 public class occur {
-
-	public static void main(String[] args) {
+      
+	public void occure(String s1,String word) {
 		int count=0;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter article");
-		String s1= sc.nextLine();
-		System.out.println("enter word");
-		String word=sc.nextLine();
-		String arr[]=s1.split(" ");
+         String arr[]=s1.split(" ");
 		
 		for(int i=0;i<arr.length;i++)
 		{
@@ -20,9 +14,17 @@ public class occur {
 		}	
 	       System.out.println("occurence of " +word +" is " +count);
 		
-		}
+}
+
+	
+	public static void main(String[] args) {
 		
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter article");
+		String s1= sc.nextLine();
+		System.out.println("enter word");
+		String word=sc.nextLine();
+		occur o=new occur();
+		o.occure(s1, word);
 	}
-
-
+}
